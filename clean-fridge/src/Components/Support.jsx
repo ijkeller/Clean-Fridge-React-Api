39b4789c57b248dd70ad752d8f9d2855e02915c9
Isaiah Keller <isaiah.keller+git@gmail.com>
2022-02-17 09:59:52 -0800

    added support component

---
+++ clean-fridge/src/Components/Support.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import List from './List';
+import Cards from './Cards/Cards';
+import Results from './Results';
+import Input from './Input';
+
+function Support() {
+    const Support = []
+
+    return (
+        <div>
+            <h2>Support</h2>
+        </div>
+    );
+}
+
+export default Support;
