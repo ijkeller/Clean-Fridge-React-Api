29420db2158ca0b402fab6a2b6a9a7d7edbaae1d
Isaiah Keller <isaiah.keller+git@gmail.com>
2022-02-18 09:08:10 -0800

    basic css for the routes

--- clean-fridge/src/Components/RecipeInfo.js
+++ clean-fridge/src/Components/RecipeInfo.js
@@ -2,12 +2,10 @@ import React from 'react'
 
 function RecipeInfo(props){
     return  (
-      <div>
-        <h1>Recipe: {props.recipe.strMeal}</h1>
-        {/* <h2>Year: {props.recipe.Year}</h2>
-        <img src={props.recipe.Poster} alt={props.recipe.Title}/>
-        <h3>Genre: {props.recipe.Genre}</h3>
-        <h4>Plot: {props.recipe.Plot}</h4> */}
+      <div className='recipe-info'>
+        <img src={props.recipe.strMealThumb} alt={props.recipe.strMeal}/>
+        <h4 id={props.recipe.idMeal}>Recipe: {props.recipe.strMeal}</h4>
+
       </div>
     )  
 }
